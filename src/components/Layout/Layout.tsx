db68b271cf7f5e17fd50e281415e83002e12b2a7
yuki384 <31533303+yuki384@users.noreply.github.com>
2023-03-03 16:59:03 +0900

    Add page transition animation

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,25 @@
 import { Sidebar } from "../Sidebar";
+import { useRouter } from "next/router";
+import { motion } from "framer-motion";
 
 interface Props {
   children: React.ReactNode;
 }
 
 export const Layout = ({ children }: Props) => {
+  const router = useRouter();
+  const { pathname } = router;
   return (
     <div className="flex bg-gray-50">
       <Sidebar />
-      <div className="w-full p-8">
+      <motion.div
+        key={pathname}
+        initial={{ opacity: 0, y: 8 }}
+        animate={{ opacity: 1, y: 0 }}
+        className="w-full p-8"
+      >
         <div className="mx-auto mt-12 max-w-[720px]">{children}</div>
-      </div>
+      </motion.div>
     </div>
   );
 };
