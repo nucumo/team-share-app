b829abb8a5fe4fc95debeb0c2bb30ad03197b0f1
yuki384 <31533303+yuki384@users.noreply.github.com>
2023-03-03 15:19:23 +0900

    Add text validation to form of comment and question

--- src/components/DailyDialog/Comment.tsx
+++ src/components/DailyDialog/Comment.tsx
@@ -1,31 +1,59 @@
 import { CommonButton } from "../Button/Common";
 import { DialogTitle } from "../Dialog/Title";
 import { TextArea } from "../Input/TextArea";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
 interface Props {
   onClickDone: () => void;
   onClickBack: () => void;
 }
 
+const schema = z.object({
+  comment: z.string().max(140, "The length must be 140 characters or fewer.").optional(),
+});
+
+type Inputs = z.infer<typeof schema>;
 export const DailyDialogComment = ({ onClickDone, onClickBack }: Props) => {
+  const { control, handleSubmit, formState } = useForm<Inputs>({
+    resolver: zodResolver(schema),
+  });
+  const onSubmit = () => {
+    onClickDone();
+  };
+
   return (
-    <div className="px-10">
-      <p className="mb-6 font-bold uppercase text-gray-400">Comment</p>
-      <div className="mb-14 pb-0.5">
-        <DialogTitle>Anything to share with the team?</DialogTitle>
-        <p className="mt-3 font-medium text-gray-400">
-          Let&apos;s write about recent events, your mind, how is your work, etc 😊
-        </p>
-      </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="px-10">
+        <p className="mb-6 font-bold uppercase text-gray-400">Comment</p>
+        <div className="mb-14 pb-0.5">
+          <DialogTitle>Anything to share with the team?</DialogTitle>
+          <p className="mt-3 font-medium text-gray-400">
+            Let&apos;s write about recent events, your mind, how is your work, etc 😊
+          </p>
+        </div>
 
-      <TextArea placeholder="e.g. It's getting warmer, so I can wake up refreshed :)" />
+        <Controller
+          name="comment"
+          control={control}
+          render={({ field, fieldState }) => (
+            <TextArea
+              {...field}
+              placeholder="e.g. It's getting warmer, so I can wake up refreshed :)"
+              isError={fieldState.invalid}
+              helperText={formState.errors.comment?.message}
+            />
+          )}
+        />
 
-      <div className={"mx-auto my-12 flex justify-center gap-4"}>
-        <CommonButton onClick={onClickBack} priority="secondary" icon="arrow-left" />
-        <CommonButton icon="arrow-right" onClick={onClickDone}>
-          Done
-        </CommonButton>
+        <div className={"mx-auto my-12 flex justify-center gap-4"}>
+          <CommonButton onClick={onClickBack} priority="secondary" icon="arrow-left" />
+          <CommonButton icon="arrow-right" type="submit" disabled={!formState.isValid}>
+            Done
+          </CommonButton>
+        </div>
       </div>
-    </div>
+    </form>
   );
 };
--- src/components/DailyDialog/Question.tsx
+++ src/components/DailyDialog/Question.tsx
@@ -1,32 +1,64 @@
 import { CommonButton } from "../Button/Common";
 import { DialogTitle } from "../Dialog/Title";
 import { TextArea } from "../Input/TextArea";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
 interface Props {
   onClickNext: () => void;
   onClickBack: () => void;
 }
 
+const schema = z.object({
+  answer: z
+    .string()
+    .min(1, "Please enter your answer.")
+    .max(140, "The length must be 140 characters or fewer."),
+});
+
+type Inputs = z.infer<typeof schema>;
+
 export const DailyDialogQuestion = ({ onClickNext, onClickBack }: Props) => {
+  const { control, handleSubmit, formState } = useForm<Inputs>({
+    resolver: zodResolver(schema),
+  });
+  const onSubmit = (data: Inputs) => {
+    onClickNext();
+  };
+
   return (
-    <div className="px-10">
-      <p className="mb-6 font-bold uppercase text-gray-400">daily question</p>
-      <div className="mb-10">
-        <DialogTitle>
-          If you could live anywhere,
-          <br />
-          what would it be?
-        </DialogTitle>
-      </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="px-10">
+        <p className="mb-6 font-bold uppercase text-gray-400">daily question</p>
+        <div className="mb-10">
+          <DialogTitle>
+            If you could live anywhere,
+            <br />
+            what would it be?
+          </DialogTitle>
+        </div>
 
-      <TextArea placeholder="e.g. country with beaches and warm weather!" />
+        <Controller
+          name="answer"
+          control={control}
+          render={({ field, fieldState }) => (
+            <TextArea
+              {...field}
+              placeholder="e.g. country with beaches and warm weather!"
+              isError={fieldState.invalid}
+              helperText={formState.errors.answer?.message}
+            />
+          )}
+        />
 
-      <div className={"mx-auto my-12 flex justify-center gap-4"}>
-        <CommonButton onClick={onClickBack} priority="secondary" icon="arrow-left" />
-        <CommonButton icon="arrow-right" onClick={onClickNext}>
-          Next
-        </CommonButton>
+        <div className={"mx-auto my-12 flex justify-center gap-4"}>
+          <CommonButton onClick={onClickBack} priority="secondary" icon="arrow-left" />
+          <CommonButton icon="arrow-right" type="submit" disabled={!formState.isValid}>
+            Next
+          </CommonButton>
+        </div>
       </div>
-    </div>
+    </form>
   );
 };
