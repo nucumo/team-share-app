b69366a2837019d129626ac9cf488e498f796f4d
yuki384 <31533303+yuki384@users.noreply.github.com>
2023-03-03 15:34:01 +0900

    Add validation to form of feeling

--- src/components/DailyDialog/Feeling.tsx
+++ src/components/DailyDialog/Feeling.tsx
@@ -2,31 +2,51 @@ import { DialogTitle } from "../Dialog/Title";
 import { FeelingChoices } from "../Feeling/Choices";
 import { CommonButton } from "../Button/Common";
 import { Feelings } from "@/constants/feelings";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schema = z.object({
+  feeling: z
+    .number()
+    .or(z.string().transform(Number))
+    .refine((val) => val > -1, "Please select one of the moods."),
+});
+
+type Inputs = z.infer<typeof schema>;
 
 interface Props {
   onClickNext: () => void;
 }
 
 export const DailyDialogFeeling = ({ onClickNext }: Props) => {
+  const { control, handleSubmit, formState } = useForm<Inputs>({
+    defaultValues: { feeling: -1 },
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = () => {
+    onClickNext();
+  };
   return (
     <>
-      <div className="mx-10 mt-10 pb-4 pt-0.5">
-        <DialogTitle>How are you feeling?</DialogTitle>
-        <div className="mt-10">
-          <FeelingChoices
-            feelings={Feelings}
-            name="feeling"
-            onChange={() => {
-              return;
-            }}
-          />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mx-10 mt-10 pb-4 pt-0.5">
+          <DialogTitle>How are you feeling?</DialogTitle>
+          <div className="mt-10">
+            <Controller
+              name="feeling"
+              control={control}
+              render={({ field }) => <FeelingChoices feelings={Feelings} {...field} />}
+            />
+          </div>
         </div>
-      </div>
-      <div className={"mx-auto my-12 flex justify-center gap-4"}>
-        <CommonButton icon="arrow-right" onClick={onClickNext}>
-          Next
-        </CommonButton>
-      </div>
+        <div className={"mx-auto my-12 flex justify-center gap-4"}>
+          <CommonButton type="submit" icon="arrow-right" disabled={!formState.isValid}>
+            Next
+          </CommonButton>
+        </div>
+      </form>
     </>
   );
 };
